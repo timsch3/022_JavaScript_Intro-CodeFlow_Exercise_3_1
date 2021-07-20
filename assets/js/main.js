bbbeafacecb3c0057d1ac949631cd108183de130
Tim Sch <timsch3@gmail.com>
2021-07-20 15:10:14 +0200

    init

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+function colors1() {
+    document.body.style.backgroundColor = "#FF3F00"
+    document.body.style.color = "#FAFF00"
+}
+function colors2() {
+    document.body.style.backgroundColor = "#B980F0"
+    document.body.style.color = "#FE9898"
+}
+function colors3() {
+    document.body.style.backgroundColor = "#0F044C"
+    document.body.style.color = "#787A91"
+}
+function colors4() {
+    document.body.style.backgroundColor = "#5D8233"
+    document.body.style.color = "#ECD662"
+}
